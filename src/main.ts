581efd351351b202135251b2869c6d420eff047b
Arman Sanasaryan <armansanasaryan91@gmail.com>
2024-11-04 16:32:12 +0400

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: (origin, callback) => {
-      const allowedOrigins = ['https://web3-calculator.vercel.app'];
+      const allowedOrigins = ['https://winners-club-front.vercel.app'];
       if (allowedOrigins.includes(origin) || !origin) {
         callback(null, true);
       } else {
