dfd06fb32e4405d173cf686fced305bb70975ed0
Arman Sanasaryan <Armansanasaryan91@gmail.com>
2024-12-23 19:52:26 +0400

    stop tracking dist folder

--- dist/general-data/dto/create-general-data.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateGeneralDataDto {
-    A242: number;
-    D244: number;
-    accumulatedBalance: number;
-    CG4: number;
-    CH4: number;
-    E242: number;
-}
--- dist/general-data/general-data.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { GeneralDataService } from './general-data.service';
-import { CreateGeneralDataDto } from './dto/create-general-data.dto';
-export declare class GeneralDataController {
-    private readonly generalDataService;
-    constructor(generalDataService: GeneralDataService);
-    createGeneralData(userId: string, createGeneralDataDto: CreateGeneralDataDto): Promise<import("./schema/general-data.schema").GeneralData>;
-    getGeneralDataByUserId(userId: string): Promise<import("./schema/general-data.schema").GeneralData>;
-    updateGeneralData(userId: string, updateGeneralDataDto: CreateGeneralDataDto): Promise<import("./schema/general-data.schema").GeneralData>;
-}
